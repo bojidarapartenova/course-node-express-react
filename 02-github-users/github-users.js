60fd5a72d00884d3ba40d68de93aa728a5871af3
bojidarapartenova <bpartenova@gmail.com>
2026-03-17 20:39:51 +0200

    github users async demo

--- 02-github-users/github-users.js
+++ 02-github-users/github-users.js
@@ -1,11 +1,13 @@
 const resultsDiv = document.getElementById('results');
 fetch('users.json')
     .then(usersResp => usersResp.json())
-    // .then(users => {
-    //     resultsDiv.innerHTML = '<ul>' + users.map(
-    //         user => `<li>${user.username}</li>`
-    //     ).join("<br>") + '</ul>';
-    //     return users;
+    // .then(users => { 
+    // resultsDiv.innerHTML = 
+    // '<ul>' +
+    // users.map(user => `<li>${user.username}</li>`)
+    // .join("<br>") 
+    // + '</ul>';
+    // return users;
     // })
     .then(users => {
         console.log(users);
@@ -15,11 +17,18 @@ fetch('users.json')
             console.log(usersData);
             const gitUsers = usersData.filter(usersData => usersData.status === 'fulfilled')
                 .map(usersData => usersData.value)
-            gitUsers.map(gitUser => {
+            const gitUserImages = gitUsers.map(gitUser => {
                 const img = new Image();
                 img.src = gitUser.avatar_url;
                 resultsDiv.append(img)
                 return img;
             })
+            return gitUserImages;
         })
+    }).then(images => new Promise(resolve =>
+        setTimeout(() => resolve(images), 10000)
+    )).then(images => {
+        images.forEach(img => {
+            img.remove();
+        });
     })
