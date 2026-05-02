bd80ecd648d6c142ded6479da9d556a1f819908a
bojidarapartenova <bpartenova@gmail.com>
2026-05-02 21:52:43 +0300

    Add Navbar

--- homework-01/cooking-app/src/App.jsx
+++ homework-01/cooking-app/src/App.jsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 const Home = () => <div>Home</div>;
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div className="container">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ homework-01/cooking-app/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useNavigate } from "react-router";
+
+const Navbar = () => {
+    const navigate = useNavigate();
+
+    const userJson = sessionStorage.getItem('loggedUser');
+    const user = userJson ? JSON.parse(userJson) : null;
+
+    const handleLogout = () => {
+        sessionStorage.removeItem('loggedUser');
+        navigate('/login');
+    };
+
+    return (
+        <nav style={{ padding: '10px', borderBottom: '1px solid #ccc', marginBottom: '20px', display: 'flex', gap: '15px' }}>
+            <Link to="/">Home</Link>
+            <Link to="/recipes">Recipes</Link>
+
+            {user ? (
+                <>
+                    <Link to="/add-recipe">Add recipe</Link>
+                    <span style={{ marginLeft: 'auto', fontWeight: 'bold' }}>
+                        Hello, {user.name}! ({user.role})
+                    </span>
+                    <button onClick={handleLogout} style={{ cursor: 'pointer' }}>Logout</button>
+                </>
+            ) : (
+                <div style={{ marginLeft: 'auto', display: 'flex', gap: '10px' }}>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Navbar;
